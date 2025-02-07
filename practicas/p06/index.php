0511a83e6a02ba908e77ef85aef5cefbde7a5965
Jahir <jahir.jfz@gmail.com>
2025-02-06 21:56:45 -0600

    primer ejercicio realizado

---
+++ practicas/p06/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC “-//W3C//DTD XHTML 1.1//EN”
+“http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd”>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang=“es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title> Titulo de la página </title>
+    </head>
+    <body>
+        <h2> 1. Escribir programa para comprobar si un número es un múltiplo de 5 y 7. </h2>
+        <?php
+
+            require_once __DIR__ . '/src/funciones.php';
+
+            if (isset($_GET['numero'])) {
+                $num= $_GET['numero'];
+                echo multiplo($num) ? "$num es múltiplo de 5 y 7" : "$num no es múltiplo de 5 y 7";
+            }
+        ?>
+    </body>
+</html>
---
+++ practicas/p06/src/funciones.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        function multiplo($num){
+            return ($num%5==0 && $num%7==0);
+        }
+    ?>
+</body>
+</html>
