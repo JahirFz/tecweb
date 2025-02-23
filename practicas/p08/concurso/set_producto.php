ad806e4f742fc956b934e634b9e615236c5dadc5
Jahir <jahir.jfz@gmail.com>
2025-02-22 20:23:57 -0600

    Archivos modificados

--- practicas/p08/concurso/set_producto.php
+++ practicas/p08/concurso/set_producto.php
@@ -8,7 +8,7 @@ $unidades = 1;
 $imagen   = 'img/imagen.png';
 
 /** SE CREA EL OBJETO DE CONEXION */
-@$link = new mysqli('localhost', 'root', '20septiembreE.', 'marketzone');	
+@$link = new mysqli('localhost', 'root', '20septiembrE.', 'marketzone');	
 
 /** comprobar la conexión */
 if ($link->connect_errno) 
