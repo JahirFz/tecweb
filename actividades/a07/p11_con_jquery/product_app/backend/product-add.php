6bc6449cbad2c10a51be778756ed8e0361e04e3b
Jahir <jahir.jfz@gmail.com>
2025-04-11 11:17:13 -0600

    Slim 4 y index.php agregados a carpeta backend

--- actividades/a07/p11_con_jquery/product_app/backend/product-add.php
+++ actividades/a07/p11_con_jquery/product_app/backend/product-add.php
@@ -2,7 +2,7 @@
 use TECWEB\MYAPI\Products as Products;
 require_once __DIR__ . '/myapi/Products.php';
 
-$products = new Products('root', '', 'marketzone');
+$products = new Products('marketzone');
 $products->add($_POST);
 echo $products->getData();
 ?>
